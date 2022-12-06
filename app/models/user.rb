3a93fef02f08963ca180052655c3dfd6a91b08e4
fukuyumi <kk.ma.yff@gmail.com>
2022-12-06 16:04:12 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+validates :nickname,
+         presence: true
+
+validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+validates :family_name,
+         format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ },
+         presence: true
+validates :first_name,
+         format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ },
+         presence: true
+validates :family_name_kana,
+         format: { with: /\A[ァ-ヶー]+\z/ },
+         presence: true
+validates :first_name_kana,
+         format: { with: /\A[ァ-ヶー]+\z/ },
+         presence: true
+
 end
