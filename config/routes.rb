24289a74efe0cd0666003cbf8b919307c7ad4c95
fukuyumi <kk.ma.yff@gmail.com>
2022-12-06 16:27:57 +0900

    user/adminのitemコントローラー、ビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'items/index'
+    get 'items/show'
+    get 'items/new'
+    get 'items/edit'
+  end
+  namespace :user do
+    get 'items/index'
+    get 'items/show'
+  end
   devise_for :admins
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
