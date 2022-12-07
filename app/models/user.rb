98ceea02f5ad63cb231ae693808a66ae64894f10
fukuyumi <kk.ma.yff@gmail.com>
2022-12-07 16:38:31 +0900

    ストロングパラメーターに追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-validates :nickname,
+validates :nick_name,
          presence: true
 
 validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
