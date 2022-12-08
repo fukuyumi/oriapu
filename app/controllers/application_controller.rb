63744b9b73faa6ffd53944a2d31610f8e13a6011
fukuyumi <kk.ma.yff@gmail.com>
2022-12-08 16:40:40 +0900

    管理者トップページとユーザートップページを分岐

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      admin_items_path
+    when User
+      root_path
+    end
+  end
+
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :family_name, :first_name, :family_name_kana, :first_name_kana])
