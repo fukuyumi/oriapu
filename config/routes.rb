6ea6b40c286ee355f4bb5c7ae43f7357492053d7
fukuyumi <kk.ma.yff@gmail.com>
2022-12-09 20:35:08 +0900

    ビュー調整

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :users
   root to: 'user/items#index'
-
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   namespace :admin do
